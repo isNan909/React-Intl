acff253d0768deec31eb633dc3d28637f533f7e7
Ishan Manandhar <ishan02016@gmail.com>
2020-05-16 15:48:25 +0545

    added simple translation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import { IntlProvider } from 'react-intl';
-// import { LocaleContext } from './LocaleContext';
-import Translate from './components/translate';
+import React, { useState } from 'react';
+import { IntlProvider, FormattedMessage } from 'react-intl';
 import './App.css';
 
+const message = {
+  en: {
+    heading: 'Welcome to our react application',
+    subheading: 'Your current launguage is {name}',
+  },
+  polish: {
+    heading: 'Witamy w naszej aplikacji react',
+    subheading: 'Twój obecny launguage jest {name}',
+  },
+};
+
 function App() {
-  // const [locale] = React.useContext(LocaleContext);
+  const [name, setName] = useState('en');
+  const [locale, setLocale] = useState('en');
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+    setLocale(e.target.value);
+  };
+
   return (
-    <IntlProvider locale="en" messages={{ en: [], nep: [] }}>
-      <div className="App">
-        <Translate />
-      </div>
-    </IntlProvider>
+    <>
+      <select onChange={handleChange} defaultValue={locale}>
+        {['en', 'polish'].map((x) => (
+          <option key={x}>{x}</option>
+        ))}
+      </select>
+
+      <IntlProvider locale={locale} messages={message[locale]}>
+        <div>
+          <FormattedMessage id="heading" values={{ name }} />
+          <br/>
+          <FormattedMessage id="subheading" values={{ name }} />
+        </div>
+      </IntlProvider>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,12 @@
+const message = {
+  en: {
+    heading: 'Hello {name}! How are you?',
+    subheading: 'The time is {t, time, short}.',
+  },
+  polish: {
+    heading: 'Hello {name}! How are you?',
+    subheading: 'The time is {t, time, short}.',
+  },
+};
+
+export default message;

c18f83230759a63dc73d3b9bd545343da17f49b6
Ishan Manandhar <ishan02016@gmail.com>
2020-05-16 16:43:35 +0545

    added simple translation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React, { useState } from 'react';
 import { IntlProvider, FormattedMessage } from 'react-intl';
-import message from './data';
 import './App.css';
 
+const message = {
+  en: {
+    heading: 'Welcome to our react application',
+    subheading: 'Simple internationalization in action',
+  },
+  de: {
+    heading: 'Willkommen zu unserer Reaktionsanwendung',
+    subheading: 'Einfache Internationalisierung in Aktion',
+  },
+};
 
 function App() {
-  const [name, setName] = useState('en');
   const [locale, setLocale] = useState('en');
 
   const handleChange = (e) => {
-    setName(e.target.value);
     setLocale(e.target.value);
   };
 
   return (
     <>
       <select onChange={handleChange} defaultValue={locale}>
-        {['en', 'polish'].map((x) => (
+        {['en', 'de'].map((x) => (
           <option key={x}>{x}</option>
         ))}
       </select>
 
       <IntlProvider locale={locale} messages={message[locale]}>
-        <div>
-          <FormattedMessage id="heading" values={{ name }} />
-          <br/>
-          <FormattedMessage id="subheading" values={{ name }} />
-        </div>
+        <p>
+          <FormattedMessage id="heading" values={{ locale }} />
+          <br />
+          <FormattedMessage id="subheading" />
+        </p>
       </IntlProvider>
     </>
   );
